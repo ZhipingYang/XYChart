87e573c98ba43df11925c9011e4c47b93ffe94e2
Zhiping Yang <XcodeYang@Gmail.com>
2015-01-26 11:42:19 +0800

    show more data on the chart
    Signed-off-by: Zhiping Yang <XcodeYang@Gmail.com>

--- UUChartView/TableViewCell.m
+++ UUChartView/TableViewCell.m
@@ -33,16 +33,45 @@
     [chartView showInView:self.contentView];
 }
 
+- (NSArray *)getXTitles:(int)num
+{
+    NSMutableArray *xTitles = [NSMutableArray array];
+    for (int i=0; i<num; i++) {
+        NSString * str = [NSString stringWithFormat:@"R-%d",i];
+        [xTitles addObject:str];
+    }
+    return xTitles;
+}
+
 #pragma mark - @required
 //横坐标标题数组
 - (NSArray *)UUChart_xLableArray:(UUChart *)chart
 {
-    if (path.row==0 && path.section==0) {
-        return @[@"one",@"two",@"three",@"four",@"five"];
-    }else if (path.row==0 && path.section==1){
-        return @[@"1",@"2",@"3",@"4",@"5",@"6",@"7",@"8",@"9",@"10",@"11",@"12",@"13"];
+
+    if (path.section==0) {
+        switch (path.row) {
+            case 0:
+                return [self getXTitles:5];
+            case 1:
+                return [self getXTitles:11];
+            case 2:
+                return [self getXTitles:7];
+            case 3:
+                return [self getXTitles:7];
+            default:
+                break;
+        }
+    }else{
+        switch (path.row) {
+            case 0:
+                return [self getXTitles:11];
+            case 1:
+                return [self getXTitles:7];
+            default:
+                break;
+        }
     }
-    return @[@"one",@"two",@"three",@"four",@"five",@"six",@"seven"];
+    return [self getXTitles:20];
 }
 //数值多重数组
 - (NSArray *)UUChart_yValueArray:(UUChart *)chart
@@ -51,14 +80,14 @@
     NSArray *ary1 = @[@"22",@"54",@"15",@"30",@"42",@"77",@"43"];
     NSArray *ary2 = @[@"76",@"34",@"54",@"23",@"16",@"32",@"17"];
     NSArray *ary3 = @[@"3",@"12",@"25",@"55",@"52"];
-    NSArray *ary4 = @[@"23",@"42",@"25",@"55",@"52",@"19",@"25",@"55",@"52",@"32",@"25",@"55",@"52"];
+    NSArray *ary4 = @[@"23",@"42",@"25",@"15",@"30",@"42",@"32",@"40",@"42",@"25",@"33"];
     
     if (path.section==0) {
         switch (path.row) {
             case 0:
                 return @[ary];
             case 1:
-                return @[ary1];
+                return @[ary4];
             case 2:
                 return @[ary1,ary2];
             default:
@@ -82,7 +111,10 @@
 //显示数值范围
 - (CGRange)UUChartChooseRangeInLineChart:(UUChart *)chart
 {
-    if (path.row==0) {
+    if (path.section==0 && (path.row==0|path.row==1)) {
+        return CGRangeMake(60, 10);
+    }
+    if (path.section==1 && path.row==0) {
         return CGRangeMake(60, 10);
     }
     if (path.row==2) {
